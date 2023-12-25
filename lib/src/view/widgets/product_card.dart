66973c625b89e504680bfcbd035a0830468fb5bc
Abdalrahman-Alhamod <abd.alrrahman.alhamod@gmail.com>
2023-12-25 04:29:33 +0300

    add mouse interaction to cards and list tile

--- lib/src/view/widgets/product_card.dart
+++ lib/src/view/widgets/product_card.dart
@@ -17,80 +17,83 @@ class ProductCard extends StatelessWidget {
       onTap: () {
         Get.off(() => ProductDetailsScreen(), arguments: product);
       },
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            height: 230,
-            width: 230,
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                  topRight: Radius.circular(8), topLeft: Radius.circular(8)),
-              color: Colors.grey.shade300,
-              image: DecorationImage(
-                image: NetworkImage(
-                  product.image,
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 230,
+              width: 230,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(8), topLeft: Radius.circular(8)),
+                color: Colors.grey.shade300,
+                image: DecorationImage(
+                  image: NetworkImage(
+                    product.image,
+                  ),
+                  fit: BoxFit.fill,
                 ),
-                fit: BoxFit.fill,
               ),
             ),
-          ),
-          Container(
-            height: 100,
-            width: 230,
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                  bottomRight: Radius.circular(8),
-                  bottomLeft: Radius.circular(8)),
-              color: theme.primaryColor,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Flexible(
-                      flex: 1,
-                      child: AutoSizeText(
-                        product.name,
-                        style: theme.textTheme.bodyLarge!.copyWith(
-                          color: Colors.white,
-                          overflow: TextOverflow.ellipsis,
+            Container(
+              height: 100,
+              width: 230,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                    bottomRight: Radius.circular(8),
+                    bottomLeft: Radius.circular(8)),
+                color: theme.primaryColor,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child: AutoSizeText(
+                          product.name,
+                          style: theme.textTheme.bodyLarge!.copyWith(
+                            color: Colors.white,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          maxLines: 1,
+                          minFontSize: 6,
                         ),
-                        maxLines: 1,
-                        minFontSize: 6,
                       ),
-                    ),
-                    Flexible(
-                      flex: 1,
-                      child: AutoSizeText(
-                        product.scientificName,
-                        style: theme.textTheme.bodyMedium!.copyWith(
-                          color: Colors.white,
-                          overflow: TextOverflow.ellipsis,
+                      Flexible(
+                        flex: 1,
+                        child: AutoSizeText(
+                          product.scientificName,
+                          style: theme.textTheme.bodyMedium!.copyWith(
+                            color: Colors.white,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          maxLines: 1,
+                          minFontSize: 6,
                         ),
-                        maxLines: 1,
-                        minFontSize: 6,
                       ),
-                    ),
-                  ],
-                ),
-                Flexible(
-                  flex: 1,
-                  child: AutoSizeText(
-                    "${product.price.toString()} ${"SP".tr}",
-                    style: theme.textTheme.bodyMedium!.copyWith(
-                        color: const Color.fromARGB(255, 181, 255, 183)),
-                    minFontSize: 8,
-                    maxLines: 1,
+                    ],
                   ),
-                ),
-              ],
+                  Flexible(
+                    flex: 1,
+                    child: AutoSizeText(
+                      "${product.price.toString()} ${"SP".tr}",
+                      style: theme.textTheme.bodyMedium!.copyWith(
+                          color: const Color.fromARGB(255, 181, 255, 183)),
+                      minFontSize: 8,
+                      maxLines: 1,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/src/view/widgets/product_list_tile.dart
+++ lib/src/view/widgets/product_list_tile.dart
@@ -26,138 +26,141 @@ class ProductListTile extends StatelessWidget {
           Get.off(() => ProductDetailsScreen(), arguments: product);
         }
       },
-      child: Container(
-          decoration: BoxDecoration(
-              border: Border.all(color: AppColors.primaryColor),
-              borderRadius: BorderRadius.circular(16)),
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey.shade300,
-                    borderRadius: BorderRadius.circular(16),
-                  ),
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: Container(
+            decoration: BoxDecoration(
+                border: Border.all(color: AppColors.primaryColor),
+                borderRadius: BorderRadius.circular(16)),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Container(
-                    width: 100,
-                    height: 100,
                     decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(product.image),
-                        fit: BoxFit.cover,
+                      color: Colors.grey.shade300,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(product.image),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: 130,
-                    child: AutoSizeText(
-                      product.name,
-                      style: theme.textTheme.titleLarge!.copyWith(
-                        color: Colors.black,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      maxLines: 1,
-                      minFontSize: 8,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 130,
-                    child: AutoSizeText(
-                      product.scientificName,
-                      style: theme.textTheme.titleMedium!.copyWith(
-                        color: Colors.black,
-                        fontWeight: FontWeight.normal,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      maxLines: 1,
-                      minFontSize: 8,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 130,
-                    child: AutoSizeText(
-                      product.brand,
-                      style: theme.textTheme.titleSmall!.copyWith(
-                        color: Colors.grey.shade600,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      maxLines: 1,
-                      minFontSize: 8,
-                    ),
-                  ),
-                ],
-              ),
-              Expanded(
-                child: Column(
+                Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    isCartProduct
-                        ? Column(
-                            children: [
-                              SizedBox(
-                                width: 130,
-                                child: CartQuantityCounter(product: product),
-                              ),
-                              const SizedBox(
-                                height: 15,
-                              ),
-                            ],
-                          )
-                        : quantity != 0
-                            ? Column(
-                                children: [
-                                  SizedBox(
-                                    width: 130,
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceEvenly,
-                                      children: [
-                                        Text(
-                                          '$quantity ${"pc".tr}',
-                                          style: theme.textTheme.titleLarge!
-                                              .copyWith(
-                                            fontWeight: FontWeight.bold,
-                                            color: theme.primaryColor,
-                                          ),
-                                          maxLines: 1,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    height: 15,
-                                  ),
-                                ],
-                              )
-                            : const SizedBox(),
                     SizedBox(
                       width: 130,
                       child: AutoSizeText(
-                        "${product.price.toString()} ${"SP".tr}",
+                        product.name,
                         style: theme.textTheme.titleLarge!.copyWith(
-                          color: const Color.fromARGB(255, 23, 171, 28),
+                          color: Colors.black,
                           overflow: TextOverflow.ellipsis,
                         ),
-                        textAlign: TextAlign.center,
                         maxLines: 1,
-                        minFontSize: 16,
+                        minFontSize: 8,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 130,
+                      child: AutoSizeText(
+                        product.scientificName,
+                        style: theme.textTheme.titleMedium!.copyWith(
+                          color: Colors.black,
+                          fontWeight: FontWeight.normal,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        maxLines: 1,
+                        minFontSize: 8,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 130,
+                      child: AutoSizeText(
+                        product.brand,
+                        style: theme.textTheme.titleSmall!.copyWith(
+                          color: Colors.grey.shade600,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        maxLines: 1,
+                        minFontSize: 8,
                       ),
                     ),
                   ],
                 ),
-              ),
-            ],
-          )
-          //visualDensity: VisualDensity(vertical: 5),
-          ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      isCartProduct
+                          ? Column(
+                              children: [
+                                SizedBox(
+                                  width: 130,
+                                  child: CartQuantityCounter(product: product),
+                                ),
+                                const SizedBox(
+                                  height: 15,
+                                ),
+                              ],
+                            )
+                          : quantity != 0
+                              ? Column(
+                                  children: [
+                                    SizedBox(
+                                      width: 130,
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          Text(
+                                            '$quantity ${"pc".tr}',
+                                            style: theme.textTheme.titleLarge!
+                                                .copyWith(
+                                              fontWeight: FontWeight.bold,
+                                              color: theme.primaryColor,
+                                            ),
+                                            maxLines: 1,
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 15,
+                                    ),
+                                  ],
+                                )
+                              : const SizedBox(),
+                      SizedBox(
+                        width: 130,
+                        child: AutoSizeText(
+                          "${product.price.toString()} ${"SP".tr}",
+                          style: theme.textTheme.titleLarge!.copyWith(
+                            color: const Color.fromARGB(255, 23, 171, 28),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          textAlign: TextAlign.center,
+                          maxLines: 1,
+                          minFontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+            //visualDensity: VisualDensity(vertical: 5),
+            ),
+      ),
     );
   }
 }
