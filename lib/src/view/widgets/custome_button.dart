bf40e27cc220bddc824c8540450f0670c0888185
Abdalrahman-Alhamod <abd.alrrahman.alhamod@gmail.com>
2023-12-25 04:31:03 +0300

    add additional optional attributes to custome button add mouse interaction

--- lib/src/view/widgets/custome_button.dart
+++ lib/src/view/widgets/custome_button.dart
@@ -9,26 +9,59 @@ class CustomeButton extends StatelessWidget {
       required this.onTap,
       this.width,
       this.height,
-      this.isEnabled = true});
+      this.isEnabled = true,
+      this.titleFontSize = 18,
+      this.backgroundColor,
+      this.icon});
   final String title;
   final double? width, height;
   final void Function() onTap;
   final bool isEnabled;
+  final double titleFontSize;
+  final Color? backgroundColor;
+  final Icon? icon;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: isEnabled ? onTap : null,
-      child: Container(
-        width: width,
-        height: height,
-        decoration: BoxDecoration(
-            color: isEnabled ? AppColors.primaryColor : Colors.grey,
-            borderRadius: const BorderRadius.all(Radius.circular(16))),
-        child: Center(
-          child: Text(
-            title,
-            style: const TextStyle(
-                color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: Container(
+          width: width,
+          height: height,
+          decoration: BoxDecoration(
+              color: isEnabled
+                  ? backgroundColor ?? AppColors.primaryColor
+                  : Colors.grey,
+              borderRadius: const BorderRadius.all(Radius.circular(16))),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              icon != null
+                  ? Row(
+                      children: [
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        icon!,
+                        const SizedBox(
+                          width: 5,
+                        )
+                      ],
+                    )
+                  : const SizedBox(),
+              // const Spacer(
+              //   flex: 1,
+              // ),
+              Text(
+                title,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: titleFontSize,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
           ),
         ),
       ),
