f84cabdc0539d08c9e94a93f1ecbdd488547d67a
Abdalrahman-Alhamod <abd.alrrahman.alhamod@gmail.com>
2023-12-28 02:14:10 +0300

    add new attribute "arName" to category model

--- lib/src/model/category.dart
+++ lib/src/model/category.dart
@@ -1,26 +1,27 @@
 class Category {
   int id;
   String name;
+  String arName;
   Category({
     required this.id,
     required this.name,
+    this.arName = "",
   });
+  
   @override
   String toString() {
     return '''
     Category {
         id: $id,
         name: $name,
+        arName: $arName
       }
     ''';
   }
 
   // JSON encoder
   Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-    };
+    return {'id': id, 'name': name, 'ar_name': arName};
   }
 
   static Map<String, dynamic> toListJson(List<Category> categories) {
@@ -37,6 +38,7 @@ class Category {
     return Category(
       id: json['id'] as int,
       name: json['name'] as String,
+      arName: json['ar_name'] ?? "",
     );
   }
 }
