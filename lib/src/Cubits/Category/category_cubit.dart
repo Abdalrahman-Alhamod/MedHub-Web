73dfac26f1f19b53f9511d79c096476c478a6f1e
Abdalrahman-Alhamod <abd.alrrahman.alhamod@gmail.com>
2023-12-28 02:10:12 +0300

    add new state and method in category cubit add delete\add\fetch category methods and states

--- lib/src/Cubits/Category/category_cubit.dart
+++ lib/src/Cubits/Category/category_cubit.dart
@@ -12,9 +12,10 @@ part 'category_state.dart';
 class CategoryCubit extends Cubit<CategoryState> {
   CategoryCubit() : super(CategoryInitial());
   List<Category>? currentCategories;
+  Category? fetchedCategory;
   Future<void> getCategories() async {
     try {
-      emit(CategoryFetchLoading());
+      emit(CategoriesFetchLoading());
 
       // Fetch Categories from API
       Map<String, dynamic> categoriesJsonData = await Api.request(
@@ -23,18 +24,96 @@ class CategoryCubit extends Cubit<CategoryState> {
           token: User.token,
           methodType: MethodType.get) as Map<String, dynamic>;
       List<Category> categories = Category.fromListJson(categoriesJsonData);
-      currentCategories = categories;
+      currentCategories = categories.toList();
       categories.insert(0, Category(id: 0, name: "All".tr));
 
       // await Future.delayed(const Duration(seconds: 2));
       // List<Category> categories = AppData.categories;
-      emit(CategoryFetchSuccess(categories: categories));
+      emit(CategoriesFetchSuccess(categories: categories));
     } on DioException catch (exception) {
       logger.e("Category Cubit Fetch : \nNetwork Failure ");
-      emit(CategoryNetworkFailure(errorMessage: exception.message.toString()));
+      emit(
+          CategoriesNetworkFailure(errorMessage: exception.message.toString()));
     } catch (e) {
       logger.e("Category Cubit Fetch : \nFetch Failure ");
+      emit(CategoriesFetchFailure(errorMessage: e.toString()));
+    }
+  }
+
+  Future<void> addCategory({required Category category, int? id}) async {
+    try {
+      emit(CategoryAddLoading());
+      String endpoint = "";
+      if (id != null) {
+        endpoint = id.toString();
+      }
+      // Fetch Categories from API
+     await Api.request(
+        url: 'admin/categories/$endpoint',
+        body: category.toJson(),
+        token: User.token,
+        methodType: MethodType.post,
+      ) as Map<String, dynamic>;
+
+
+      // await Future.delayed(const Duration(seconds: 2));
+      // List<Category> categories = AppData.categories;
+      emit(CategoryAddSuccess());
+    } on DioException catch (exception) {
+      logger.e("Category Cubit Add : \nNetwork Failure ");
+      emit(CategoryAddNetworkFailure(
+          errorMessage: exception.message.toString()));
+    } catch (e) {
+      logger.e("Category Cubit Add : \nFetch Failure ");
+      emit(CategoryAddFailure(errorMessage: e.toString()));
+    }
+  }
+
+  Future<void> getCategory({required int id}) async {
+    try {
+      emit(CategoryFetchLoading());
+
+      // Fetch Categories from API
+      Map<String, dynamic> categoryJsonData = await Api.request(
+          url: 'admin/categories/$id',
+          body: {},
+          token: User.token,
+          methodType: MethodType.get) as Map<String, dynamic>;
+
+      fetchedCategory = Category.fromJson(categoryJsonData['category']);
+
+      emit(CategoryFetchSuccess());
+    } on DioException catch (exception) {
+      logger.e("Category Cubit Fetch : \nNetwork Failure ");
+      emit(CategoryFetchNetworkFailure(
+          errorMessage: exception.message.toString()));
+    } catch (e) {
+      logger.e("Category Cubit Fetch : \nFetch Failure \n${e.toString()}");
       emit(CategoryFetchFailure(errorMessage: e.toString()));
+    }
+  }
+
+  Future<void> deleteCategory({required int id}) async {
+    try {
+      emit(CategoryDeleteLoading());
+      await Api.request(
+        url: 'admin/categories/$id',
+        body: {},
+        token: User.token,
+        methodType: MethodType.delete,
+      ) as Map<String, dynamic>;
+
+
+      // await Future.delayed(const Duration(seconds: 2));
+      // List<Category> categories = AppData.categories;
+      emit(CategoryDeleteSuccess());
+    } on DioException catch (exception) {
+      logger.e("Category Cubit Delete : \nNetwork Failure ");
+      emit(CategoryDeleteNetworkFailure(
+          errorMessage: exception.message.toString()));
+    } catch (e) {
+      logger.e("Category Cubit Delete : \nFetch Failure ");
+      emit(CategoryDeleteFailure(errorMessage: e.toString()));
     }
   }
 }
--- lib/src/Cubits/Category/category_state.dart
+++ lib/src/Cubits/Category/category_state.dart
@@ -4,19 +4,61 @@ class CategoryState {}
 
 final class CategoryInitial extends CategoryState {}
 
+final class CategoriesFetchLoading extends CategoryState {}
+
+final class CategoriesFetchSuccess extends CategoryState {
+  List<Category> categories;
+  CategoriesFetchSuccess({required this.categories});
+}
+
+final class CategoriesFetchFailure extends CategoryState {
+  final String errorMessage;
+  CategoriesFetchFailure({required this.errorMessage});
+}
+
+final class CategoriesNetworkFailure extends CategoryState {
+  final String errorMessage;
+  CategoriesNetworkFailure({required this.errorMessage});
+}
+
 final class CategoryFetchLoading extends CategoryState {}
 
-final class CategoryFetchSuccess extends CategoryState {
-  List<Category> categories;
-  CategoryFetchSuccess({required this.categories});
-}
+final class CategoryFetchSuccess extends CategoryState {}
 
 final class CategoryFetchFailure extends CategoryState {
   final String errorMessage;
   CategoryFetchFailure({required this.errorMessage});
 }
 
-final class CategoryNetworkFailure extends CategoryState {
+final class CategoryFetchNetworkFailure extends CategoryState {
   final String errorMessage;
-  CategoryNetworkFailure({required this.errorMessage});
+  CategoryFetchNetworkFailure({required this.errorMessage});
+}
+
+final class CategoryAddLoading extends CategoryState {}
+
+final class CategoryAddSuccess extends CategoryState {}
+
+final class CategoryAddFailure extends CategoryState {
+  final String errorMessage;
+  CategoryAddFailure({required this.errorMessage});
+}
+
+final class CategoryAddNetworkFailure extends CategoryState {
+  final String errorMessage;
+  CategoryAddNetworkFailure({required this.errorMessage});
+}
+
+final class CategoryDeleteLoading extends CategoryState {}
+
+final class CategoryDeleteSuccess extends CategoryState {}
+
+final class CategoryDeleteFailure extends CategoryState {
+  final String errorMessage;
+  CategoryDeleteFailure({required this.errorMessage});
+}
+
+final class CategoryDeleteNetworkFailure extends CategoryState {
+  final String errorMessage;
+  CategoryDeleteNetworkFailure({required this.errorMessage});
 }
