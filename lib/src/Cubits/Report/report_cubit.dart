f4507eab68719d0c8356670d8847f4950ec226ac
Abdalrahman-Alhamod <abd.alrrahman.alhamod@gmail.com>
2023-12-30 19:23:26 +0300

    create report cubit add fetch report method

---
+++ lib/src/Cubits/Report/report_cubit.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../main.dart';
+import '../../model/user.dart';
+import '../../services/api.dart';
+
+part 'report_state.dart';
+
+class ReportCubit extends Cubit<ReportState> {
+  ReportCubit() : super(ReportInitial());
+  void getReport(
+      {required DateTime startDate, required DateTime endDate}) async {
+    try {
+      emit(ReportFetchLoading());
+      Map<String, dynamic> reportJsonData = await Api.request(
+        url:
+            'admin/pdf/${startDate.year}/${startDate.month}/${startDate.day}/${endDate.year}/${endDate.month}/${endDate.day}',
+        body: {},
+        token: User.token,
+        methodType: MethodType.get,
+        lang: 'en',
+      ) as Map<String, dynamic>;
+      emit(ReportFetchSuccess(pdfLink: reportJsonData['file']));
+    } on DioException catch (exception) {
+      logger.e("Report Cubit : \nNetwork Failure \n${exception.message}");
+      emit(ReportFetchNetworkFailure(
+          errorMessage: exception.message.toString()));
+    } catch (e) {
+      logger.e("Report Cubit : \nFetch Failure \n$e");
+      emit(ReportFetchFailure(errorMessage: e.toString()));
+    }
+  }
+}
---
+++ lib/src/Cubits/Report/report_state.dart
@@ -0,0 +1,22 @@
+part of 'report_cubit.dart';
+
+final class ReportState {}
+
+final class ReportInitial extends ReportState {}
+
+final class ReportFetchLoading extends ReportState {}
+
+final class ReportFetchSuccess extends ReportState {
+  String pdfLink;
+  ReportFetchSuccess({required this.pdfLink});
+}
+
+final class ReportFetchFailure extends ReportState {
+  String errorMessage;
+  ReportFetchFailure({required this.errorMessage});
+}
+
+final class ReportFetchNetworkFailure extends ReportState {
+  String errorMessage;
+  ReportFetchNetworkFailure({required this.errorMessage});
+}
